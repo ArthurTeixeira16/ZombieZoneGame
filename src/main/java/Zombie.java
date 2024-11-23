cf12ecba7798f9611e6eb8840d455357fca6c89f
Pedro Gouveia <p.gouveia2001@gmail.com>
2024-11-23 18:09:09 +0000

    Zumbis e soldado implementados sem movimento

---
+++ src/main/java/Zombie.java
@@ -0,0 +1,23 @@
+import com.googlecode.lanterna.TextColor;
+import com.googlecode.lanterna.graphics.TextGraphics;
+
+public class Zombie extends Element{
+
+    public Zombie(int x,int y){
+        super(x,y);
+    }
+
+    @Override
+    public Position getPosition() {
+        return super.getPosition();
+    }
+
+    public void draw(TextGraphics screen) {
+        int baseX = getPosition().getX();
+        int baseY = getPosition().getY();
+        screen.putString(baseX, baseY, "  Z");
+        screen.putString(baseX, baseY+1, " /|\\");
+        screen.putString(baseX, baseY+2, " / \\");
+        screen.setForegroundColor(TextColor.Factory.fromString("#FF0000"));
+    }
+}
