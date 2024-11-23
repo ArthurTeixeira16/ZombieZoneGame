44a35d91797e1fb657a8315532efd5d384c10d20
Pedro Gouveia <p.gouveia2001@gmail.com>
2024-11-23 18:06:53 +0000

    Fix problemas com os pushs do divas

--- src/main/java/Arena.java
+++ src/main/java/Arena.java
@@ -1,5 +1,8 @@
 import com.googlecode.lanterna.TextColor;
 import com.googlecode.lanterna.graphics.TextGraphics;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class Arena {
@@ -7,12 +10,15 @@ public class Arena {
     private int width;
     private Random random;
     private Soldier soldier;
+    private List<Zombie> zombies;
 
     public Arena(int width, int height) {
         this.width = width;
         this.height = height;
         this.random = new Random();
-        this.soldier = new Soldier(width/2,height/2);
+        this.soldier = new Soldier(width/2,height/2);//c
+        this.zombies = SpawnZombies();
+
     }
 
     public void draw(TextGraphics screen) {
@@ -27,10 +33,28 @@ public class Arena {
             }
         }
         soldier.draw(screen);
+        for(Zombie zombie:zombies){
+            zombie.draw(screen);
+        }
     }
     private TextColor randomGrayShade() {
         int grayValue = 180 + random.nextInt(50);
         String hexGray = String.format("#%02X%02X%02X", grayValue, grayValue, grayValue);
         return TextColor.Factory.fromString(hexGray);
+    }
+    private List<Zombie> SpawnZombies(){
+        List<Zombie> zombies = new ArrayList<>();
+        int i = 0;
+        while(i < 5) {
+            int num1,num2;
+            do { num1 = random.nextInt(height-2) + 1;
+            } while( num1 == width/2);
+            do { num2 = random.nextInt(width-2) + 1;
+            } while( num2 == height/2);
+            Zombie zombie = new Zombie(num1,num2);
+            zombies.add(zombie);
+            i++;
+        }
+        return zombies;
     }
 }
